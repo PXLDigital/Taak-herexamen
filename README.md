ef43ab1e8573f6fd3f5d88b4efc1e82164d20f1d
monsterseppe <99758377+monsterseppe@users.noreply.github.com>
2022-08-04 12:26:29 +0200

    intput output with dummy protection

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 hoe werkt het programma
 
 1. compile de c code
-2. run het progamma in de cmd op deze manier naam van het programma dan de input file en dan de output file (name.exe input.bmp output.bmp)
+2. run het progamma in de cmd het verwacht programma naam  een input in de vorm van -in filename en een output in de vorm van  -out filename (opdracth -in input.bmp -out output)
 3. stell u filter waarden in tussen 0-255 voor elke kleur
 4. zie de gefilterde foto  bij u output naam
 
 bronnen :
 
 basis code : https://github.com/SeppeBudenaers/smooth-filter (ben uit mijn vorige taak gestart)
+string.h : https://www.tutorialspoint.com/c_standard_library/string_h.htm 
--- opdracht.c
+++ opdracht.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-void filter(unsigned char * pixels, signed int hoogte, signed int breedte);
+#include <string.h>
+void filter(unsigned char *pixels, signed int hoogte, signed int breedte);
 void cleanup(unsigned char *pixels, unsigned char *header, int totaalAantalPixels, const char *argv);
 
 int main(int argc, char const *argv[])
 {
+    int input = 0;
+    int output = 0 ;
+    for (int i = 0; i < argc; i++)
+    {
+             if (strcmp(argv[i], "-in") == 0)
+        {
+            input = i + 1;
+            break;
+        }
+    }
+    if (input == 0)
+    {
+        printf("could not find -in");
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i = 0; i < argc; i++)
+    {
+             if (strcmp(argv[i], "-out") == 0)
+        {
+            output = i + 1;
+            break;
+        }
+    }
+   if (output == 0)
+    {
+        printf("could not find -out");
+        exit(EXIT_FAILURE);
+    }
     // opening file
-    FILE *inputBMP = fopen(argv[1], "rb");
+    FILE *inputBMP = fopen(argv[input], "rb");
     unsigned char header[54] = {0};
     signed int hoogte = 0;
     signed int breedte = 0;
@@ -38,75 +68,85 @@ int main(int argc, char const *argv[])
     printf("INFO: Heap memory allocated = %d (bytes)\n", totaalAantalPixels * 3);
 
     fclose(inputBMP);
-    printf("INFO: File %s CLOSED\n", argv[1]);
+    printf("INFO: File %s CLOSED\n", argv[input]);
 
     //----------------------------------------
-    filter(pixels,hoogte,breedte);
-    cleanup(pixels, header, totaalAantalPixels, argv[2]);
+    filter(pixels, hoogte, breedte);
+    cleanup(pixels, header, totaalAantalPixels, argv[output]);
 
     //-----------------------------------------
 }
 
-void filter(unsigned char * pixels, signed int hoogte, signed int breedte){
+void filter(unsigned char *pixels, signed int hoogte, signed int breedte)
+{
     int Rf = 0;
     int Gf = 0;
     int Bf = 0;
     while (1)
     {
         printf("Rood filter geef een getal (0 - 255) :");
-        scanf("%i",&Rf);
+        scanf("%i", &Rf);
         if (Rf >= 0 && Rf <= 255)
         {
             break;
         }
         else
         {
-            printf("%i zit niet tussen 0 en 255\n",Rf);
+            printf("%i zit niet tussen 0 en 255\n", Rf);
         }
     }
     while (1)
     {
         printf("Groen filter geef een getal (0 - 255) :");
-        scanf("%i",&Gf);
+        scanf("%i", &Gf);
         if (Gf >= 0 && Gf <= 255)
         {
             break;
         }
         else
         {
-            printf("%i zit niet tussen 0 en 255\n",Gf);
+            printf("%i zit niet tussen 0 en 255\n", Gf);
         }
     }
     while (1)
     {
         printf("Blauw filter geef een getal (0 - 255) :");
-        scanf("%i",&Bf);
+        scanf("%i", &Bf);
         if (Bf >= 0 && Bf <= 255)
         {
             break;
         }
         else
         {
-            printf("%i zit niet tussen 0 en 255\n",Bf);
+            printf("%i zit niet tussen 0 en 255\n", Bf);
         }
     }
-    
+
     for (int y = 0; y < hoogte; ++y)
     {
         for (int x = 0; x < breedte; ++x)
         {
             long startloc = (x + (y * breedte));
             startloc *= 3;
-            if (Rf < pixels[startloc + 2]) {pixels[startloc + 2] = Rf;} //Rood
+            if (Rf < pixels[startloc + 2])
+            {
+                pixels[startloc + 2] = Rf;
+            } // Rood
 
-            if (Gf < pixels[startloc + 1]) {pixels[startloc + 1] = Gf;} //
-            
-            if (Bf < pixels[startloc]) {pixels[startloc] = Bf;}// blauw
+            if (Gf < pixels[startloc + 1])
+            {
+                pixels[startloc + 1] = Gf;
+            } //
+
+            if (Bf < pixels[startloc])
+            {
+                pixels[startloc] = Bf;
+            } // blauw
         }
     }
 }
 
-void cleanup(unsigned char *pixels, unsigned char *header, int totaalAantalPixels, const char * output)
+void cleanup(unsigned char *pixels, unsigned char *header, int totaalAantalPixels, const char *output)
 {
     FILE *fpw = fopen(output, "wb");
     if (fpw == NULL)
